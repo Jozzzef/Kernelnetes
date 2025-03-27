245b92b412e7826882d25dd4089338ae4fd84632
Jozef Lumaj <jozef.lumaj@gmail.com>
2025-03-26 23:24:35 -0400

    init

--- kernelnetes/src/apps/add_scalars.rs
+++ kernelnetes/src/apps/add_scalars.rs
@@ -1,11 +1,16 @@
 use opencl3::program::Program;
 use opencl3::context::Context;
+use opencl3::command_queue::CommandQueue;
+use opencl3::kernel::Kernel;
+use opencl3::memory::{Buffer, ClMem};
 
-pub fn add_scalars() {
+pub fn add_scalars(ctx: &Context) {
     println!("add_scalar");
     const FILE_LOC: &str = "./kernels/add_scalars.c";
+    let cq = create_command_queue(ctx);
+    let prog = compile_program(FILE_LOC, ctx);
+    let kernel = get_kernel(&prog, "add_scalars");
 }
-
 
 pub fn compile_program(path_to_file: &str, ctx: &Context) -> Program {
     let program_source = std::fs::read_to_string(path_to_file)
@@ -13,6 +18,10 @@ pub fn compile_program(path_to_file: &str, ctx: &Context) -> Program {
     Program::create_and_build_from_source(ctx, program_source.as_str(), "").unwrap()
 }
 
-pub fn create_command_queue(){
+pub fn create_command_queue(ctx: &Context){
+    CommandQueue::create_default_with_properties(ctx, 0, 0).unwrap();
+}
 
+pub fn get_kernel(program: &Program, kern_name: &str){
+    Kernel::create(program, kern_name).expect("Kernel::create failed");
 }
--- kernelnetes/src/main.rs
+++ kernelnetes/src/main.rs
@@ -4,10 +4,10 @@ use apps::add_scalars::add_scalars;
 
 fn main() {
     // Setup host environment
-    host::easy_setup();
+    let ctx = host::easy_setup();
     // Do actual execution (command queues -> programs -> kernels)
     // in apps folder is where you put your code
-    add_scalars();
+    add_scalars(&ctx);
 }
 
 
