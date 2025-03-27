a3546b9e463408dd13a5ec6a34619db0fea6e903
Jozef Lumaj <jozef.lumaj@gmail.com>
2025-03-26 21:20:36 -0400

    init

---
+++ kernelnetes/target/debug/deps/dlopen2_derive-e70ab5746421a173.d
@@ -0,0 +1,9 @@
+/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/deps/libdlopen2_derive-e70ab5746421a173.so: /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/lib.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/api.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/common.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/multi_api.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/wrapper.rs
+
+/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/deps/dlopen2_derive-e70ab5746421a173.d: /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/lib.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/api.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/common.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/multi_api.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/wrapper.rs
+
+/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/lib.rs:
+/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/api.rs:
+/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/common.rs:
+/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/multi_api.rs:
+/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dlopen2_derive-0.4.0/src/wrapper.rs:
---
+++ kernelnetes/target/debug/deps/kernelnetes-2d35444e3427eeb5.d
@@ -0,0 +1,9 @@
+/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/deps/libkernelnetes-2d35444e3427eeb5.rmeta: src/main.rs Cargo.toml
+
+/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/deps/kernelnetes-2d35444e3427eeb5.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
