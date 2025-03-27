0410ec94fa67987590046008ee85dd7edbc233b2
Jozef Lumaj <47994714+Jozzzef@users.noreply.github.com>
2025-03-26 23:31:25 -0400

    Delete kernelnetes/target directory

--- kernelnetes/target/debug/deps/thiserror-b6922c230299409e.d
+++
@@ -1,8 +0,0 @@
-/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/deps/libthiserror-b6922c230299409e.rmeta: /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/lib.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/aserror.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/display.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/var.rs
-
-/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/deps/thiserror-b6922c230299409e.d: /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/lib.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/aserror.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/display.rs /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/var.rs
-
-/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/lib.rs:
-/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/aserror.rs:
-/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/display.rs:
-/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/thiserror-2.0.12/src/var.rs:
