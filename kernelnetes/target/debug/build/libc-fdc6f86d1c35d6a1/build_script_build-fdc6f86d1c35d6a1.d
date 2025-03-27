102d679400b10be493372ce4530557e3241252e8
Jozef Lumaj <jozef.lumaj@gmail.com>
2025-03-26 21:23:06 -0400

    init

---
+++ kernelnetes/target/debug/build/libc-fdc6f86d1c35d6a1/build_script_build-fdc6f86d1c35d6a1.d
@@ -0,0 +1,5 @@
+/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/build/libc-fdc6f86d1c35d6a1/build_script_build-fdc6f86d1c35d6a1: /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.171/build.rs
+
+/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/build/libc-fdc6f86d1c35d6a1/build_script_build-fdc6f86d1c35d6a1.d: /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.171/build.rs
+
+/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.171/build.rs:
---
+++ kernelnetes/target/debug/build/opencl-sys-46f66a0cacbefe7f/build_script_build-46f66a0cacbefe7f.d
@@ -0,0 +1,5 @@
+/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/build/opencl-sys-46f66a0cacbefe7f/build_script_build-46f66a0cacbefe7f: /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/opencl-sys-0.5.0/build.rs
+
+/home/jozef/Desktop/Kernelnetes/kernelnetes/target/debug/build/opencl-sys-46f66a0cacbefe7f/build_script_build-46f66a0cacbefe7f.d: /home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/opencl-sys-0.5.0/build.rs
+
+/home/jozef/.cargo/registry/src/index.crates.io-6f17d22bba15001f/opencl-sys-0.5.0/build.rs:
